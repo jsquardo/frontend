11acdff6947d8904036c76d9f691455de41ec832
Johnny Squardo <johnny.squardo@howardcenter.com>
2022-11-04 17:25:13 -0400

    Setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from 'vite-tsconfig-paths'
+import checker from 'vite-plugin-checker'
 
 export default defineConfig(({ mode }) => {
   /**
@@ -56,7 +57,7 @@ export default defineConfig(({ mode }) => {
   }
 
   return {
-    plugins: [tsconfigPaths(), react()],
+    plugins: [tsconfigPaths(), react(), checker({ typescript: true })],
 
     base: "/",
     publicDir: "./public",
